1aa9612f65592b4cda5e2be50a0e3e6d8b54905f
Onwuchekwa Okechukwu <xcellnximo57@gmail.com>
2022-10-04 00:39:31 +0100

    implement feat login

---
+++ src/api/axios.js
@@ -0,0 +1,15 @@
+
+import axios from "axios";
+
+let headers = {};
+if (localStorage.token) {
+    console.log(localStorage.token);
+  headers.Authorization = `Bearer ${localStorage.token}`;
+}
+
+export default axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+    headers
+});
+
+
